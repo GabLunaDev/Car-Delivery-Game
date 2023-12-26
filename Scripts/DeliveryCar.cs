aea91a8d815563ffbad8fc486ed9e6cccc0970a9
GabLunaDev <gabriellunacv@gmail.com>
2023-12-26 20:49:50 -0300

    Improving physics of delivery car code

--- Scripts/DeliveryCar.cs
+++ Scripts/DeliveryCar.cs
@@ -1,41 +1,68 @@
+using System;
 using Godot;
 
 public partial class DeliveryCar : CharacterBody2D
 {
-    [Export]
-    public int Speed { get; set; } = 400;
+    [Export] public float EnginePower { get; set; } = 800f;
+    [Export] public float Braking { get; set; } = -450f;
+    [Export] public float MaxSpeedReverse { get; set; } = 250f;
+    [Export] public float Friction { get; set; } = -0.9f;
+    [Export] public float Drag { get; set; } = -0.0015f;
 
-    [Export]
-    public float RotationSpeed { get; set; } = 0.5f;
 
-    private float _rotationDirection;
+    private float _steeringAngle = 20f;
+    private float _wheelBase = 177.5f;
+    private Vector2 _velocity = Vector2.Zero;
+    private Vector2 _acceleration = Vector2.Zero;
+    private float _steerDirection;
     private float _forwardDirection;
 
-    public void GetInput()
+    private void GetInput()
     {
-        _rotationDirection = Input.GetAxis("move_left", "move_right");
-        _forwardDirection = Input.GetAxis("move_backward", "move_forward");
+        float turnDirection = Input.GetAxis("move_left", "move_right");
+        _steerDirection = turnDirection * _steeringAngle;
+        if(Input.IsActionPressed("move_forward")) 
+            _acceleration = Transform.X * EnginePower * Input.GetActionStrength("move_forward");
+        if(Input.IsActionPressed("move_backward"))
+            _acceleration = Transform.X * Braking * Input.GetActionStrength("move_backward");
+
+    }
+
+    private void CalculateSteering(double delta)
+    {
+        var rear_wheel = Position - Transform.X * _wheelBase/2;
+        var front_wheel = Position + Transform.X * _wheelBase/2;
+
+        rear_wheel += Velocity * (float)delta;
+        front_wheel += Velocity.Rotated(_steerDirection) * (float)delta;
+        var new_heading = (front_wheel - rear_wheel).Normalized();
+        var d = new_heading.Dot(Velocity.Normalized());
+        if(d > 0)
+            Velocity = new_heading * Velocity.Length();
+        if(d < 0)
+            Velocity = -new_heading * Math.Min(Velocity.Length(), MaxSpeedReverse);
+        Rotation = new_heading.Angle();
+    }
+
+    private void ApplyFriction()
+    {
+        if(Velocity.Length() < 5)
+            Velocity = Vector2.Zero;
+        var frictionForce = Velocity * Friction;
+        var dragForce = Velocity * Velocity.Length() * Drag;
+
+        if(Velocity.Length() < 100)
+            frictionForce *= 3;
+        _acceleration += dragForce + frictionForce;
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        _acceleration = Vector2.Zero;
         GetInput();
-
-        int SpeedValue = 0;
-
-        if(_forwardDirection > 0)
-            SpeedValue = Speed;
-            if(Velocity.X != 0)
-                Rotation += _rotationDirection * RotationSpeed * 3 * (float)delta;    
-        if(_forwardDirection < 0)
-            SpeedValue = Speed/2;
-            if(Velocity.X != 0)
-                Rotation += _rotationDirection * RotationSpeed * 0.5f * (float)delta;
-
-        Velocity = Transform.X * _forwardDirection * SpeedValue;
-
-        GD.Print(Rotation);
-        
+        CalculateSteering(delta);
+        ApplyFriction();
+        Velocity += _acceleration * (float)delta;
         MoveAndSlide();
     }
 }
